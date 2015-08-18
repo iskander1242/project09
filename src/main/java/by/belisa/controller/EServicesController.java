8241a36ba2228d03b6daaf5f32ccc81b4d277597
iskander1242 <iskander1240@gmail.com>
2015-08-18 20:43:46 +0300

    222

--- src/main/java/by/belisa/controller/EServicesController.java
+++ src/main/java/by/belisa/controller/EServicesController.java
@@ -1,29 +1,162 @@
 package by.belisa.controller;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 
-import org.hibernate.criterion.Criterion;
+import javax.portlet.ActionRequest;
+import javax.portlet.PortletRequest;
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
+
+import org.apache.log4j.Logger;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.bind.annotation.ActionMapping;
+import org.springframework.web.portlet.bind.annotation.RenderMapping;
+import org.springframework.web.portlet.bind.annotation.ResourceMapping;
 
-import by.belisa.entity.NewTech;
-import by.belisa.entity.Services;
+import by.belisa.bean.AnketaDTO;
+import by.belisa.entity.ANO;
+import by.belisa.entity.OrgInfo;
+import by.belisa.entity.Organization;
+import by.belisa.entity.UchStepeni;
+import by.belisa.entity.UchZvaniy;
+import by.belisa.entity.User;
+import by.belisa.exception.DaoException;
 import by.belisa.exception.ServiceException;
-import by.belisa.service.EServicesService;
+import by.belisa.service.AnketaService;
+import by.belisa.service.OrgService;
+import by.belisa.service.UchStepeniService;
+import by.belisa.service.UchZvaniyService;
+import by.belisa.service.UserService;
 
-@Controller
-@RequestMapping("VIEW")
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.util.PortalUtil;
+
+
+@RequestMapping(value = "VIEW")
+// @SessionAttributes("anketa")
 public class EServicesController {
+	private static Logger log = Logger.getLogger(EServicesController.class);
+
 	@Autowired
-	@Qualifier("eServicesService")
-	private EServicesService eServicesService;
-	@RequestMapping
-	public String renderView(Model model) throws ServiceException{
-		List<Services> newTechList =eServicesService.getAllbyPublication(new Long(1));	
-		model.addAttribute("newTechList", newTechList);
+	@Qualifier("userService")
+	private UserService userService;
+	@Autowired
+	@Qualifier("anketaService")
+	private AnketaService anketaService;
+	@Autowired
+	@Qualifier("uchStepeniService")
+	private UchStepeniService uchStepeniService;
+	@Autowired
+	@Qualifier("uchZvaniyService")
+	private UchZvaniyService uchZvaniyService;
+	@Autowired
+	@Qualifier("orgService")
+	private OrgService orgService;	
+
+	@RenderMapping
+	public String renderView(Model model, PortletRequest request)
+			throws ServiceException, DaoException {
+		AnketaDTO anketaDTO = null;
+		try {
+			com.liferay.portal.model.User user = PortalUtil.getUser(request);
+			if (user != null) {
+				long pk = user.getPrimaryKey();
+				anketaDTO = anketaService.getDTO(pk);
+				if (anketaDTO.getId() == 0) {
+					anketaDTO.setEmail(user.getEmailAddress());
+//					anketaDTO.setFullFio(user.getLastName() + " "
+//							+ user.getFirstName() + " " + user.getMiddleName());
+//					anketaDTO.setFio(user.getLastName() + " "
+//							+ user.getFirstName().charAt(0) + "."
+//							+ user.getMiddleName().charAt(0) + ".");
+					anketaDTO.setName(user.getFirstName());
+					anketaDTO.setSurname(user.getLastName());
+					anketaDTO.setPatronymic(user.getMiddleName());
+					SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+					anketaDTO.setBirthday(df.format(user.getBirthday()));
+				}
+
+			} else {
+				anketaDTO = new AnketaDTO();
+			}
+
+		} catch (PortalException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SystemException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		List<UchStepeni> uchStepeniList = uchStepeniService.getAll();
+		List<UchZvaniy> uchZvaniyList = uchZvaniyService.getAll();
+		List<Organization> orgList = orgService.getAll();
+		model.addAttribute("uchStepeniList", uchStepeniList);
+		model.addAttribute("uchZvaniyList", uchZvaniyList);
+		model.addAttribute("orgList", orgList);
+		model.addAttribute("anketa", anketaDTO);
 		return "eServices";
 	}
+	
+	@ActionMapping
+	public void getFormData1(@ModelAttribute AnketaDTO anketaDTO,
+			ActionRequest aRequest, Model model) throws ServiceException,
+			PortalException, SystemException, ParseException, DaoException {
+//		long pk = PortalUtil.getUser(aRequest).getUserId();
+//		anketaDTO.setId(pk);
+//		anketaDTO.setFullFio(anketaDTO.getSurname() + " "
+//		+ anketaDTO.getName() + " " + anketaDTO.getPatronymic());
+//		anketaDTO.setFio(anketaDTO.getSurname() + " "
+//		+ anketaDTO.getName().charAt(0) + "."
+//		+ anketaDTO.getPatronymic().charAt(0) + ".");
+//		anketaService.saveOrUpdate(anketaDTO);
+//		User user = userService.get(pk);
+//		
+//		user.setFirstName(anketaDTO.getName());
+//		user.setMiddleName(anketaDTO.getPatronymic());
+//		user.setLastName(anketaDTO.getSurname());
+//		user.setGreeting("Welcome "+anketaDTO.getSurname()+" "+anketaDTO.getName());
+//		userService.update(user);
+		model.addAttribute("save_result", "ok");
+	}
+	
+	@ResourceMapping
+	public void checkUnp(ResourceRequest req, ResourceResponse resp) throws IOException{
+		String unp = req.getParameter("unp");
+		
+		     ANO ano = new ANO();
+		     OrgInfo oi=new OrgInfo();
+		     oi.setCity("Минск11111");
+		     oi.setNameRus("ИППС11111");
+		     ano.setOrg(oi);
+		 
+			 ANO ano1 = new ANO();
+			 OrgInfo oi1=new OrgInfo();
+			 oi1.setCity("Минск1");
+			 oi1.setNameRus("ИППС1");
+			 ano1.setOrg(oi1);
+			 
+			 List<ANO> arr=new ArrayList<ANO>();
+			 
+			 arr.add(ano1);
+			 arr.add(ano);			 
+		   
+		try (OutputStream outStream = resp.getPortletOutputStream()){
+			ObjectMapper mapper = new ObjectMapper();
+			String jsonString = mapper.writeValueAsString(arr);
+			outStream.write(jsonString.getBytes());
+		}
+	}
+
 }
--- src/main/webapp/WEB-INF/jsp/eServices.jsp
+++ src/main/webapp/WEB-INF/jsp/eServices.jsp
@@ -1,3 +1,19 @@
+<%--
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+--%>
+
 <%@ page contentType="text/html" language="java" pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/portlet_2_0" prefix="portlet"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
@@ -7,38 +23,112 @@
 <%@ taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
 
 <portlet:defineObjects />
-
-
+<portlet:actionURL var="sendForm">
+</portlet:actionURL>
 <c:set var="ns">
 	<portlet:namespace />
 </c:set>
+<aui:form method="POST" action="${sendForm}" name="anketa">
+	<aui:input name="birthday" label="DFROM" value="${anketa.birthday}"
+		bean="anketa">
+		<aui:validator name="required" />
+		<aui:validator name="custom" errorMessage="Формат даты дд-мм-гггг">
+						function (val, fieldNode, ruleValue) {
+							var result = false;
+							
+							if (val == '' || val.search(/^\d{2}-\d{2}-\d{4}$/)!=-1){
+								result = true;
+							}
+							return result;
+						}
+		</aui:validator>
+	</aui:input>
+	<aui:input name="birthday" label="DTO" value="${anketa.birthday}"
+		bean="anketa">
+		<aui:validator name="required" />
+		<aui:validator name="custom" errorMessage="Формат даты дд-мм-гггг">
+						function (val, fieldNode, ruleValue) {
+							var result = false;
+							
+							if (val == '' || val.search(/^\d{2}-\d{2}-\d{4}$/)!=-1){
+								result = true;
+							}
+							return result;
+						}
+		</aui:validator>
+	</aui:input>
+	<aui:select name="orgId" class="chosen" label="Организация">
+		<aui:option value=""></aui:option>
+		<c:forEach var="i" items="${orgList}">
+			<aui:option value="${i.id}" label="${i.name}"
+				selected="${i.id==anketa.orgId}"></aui:option>
+		</c:forEach>
+	</aui:select>
+	<br />
 
-<div class="paging"></div>
-<table id="newTech-table" class="table table-bordered table-striped table-hover" style="table-layout: fixed">
-<thead>
-	<tr>		
-		<th>Наименование</th>
-		<th>Описание услуги</th>
-	</tr>
-	</thead>
-	<tbody>
-	<c:forEach var="item" items="${newTechList}">
+
+	<aui:button-row>
+		<div align="left">
+			<aui:button type="submit" value="Показать данные" />
+
+		</div>
+	</aui:button-row>
+	<table>
 		<tr>
-			<td class="col-md-2 span2"><div style="font-size:small">${item.name}<input type="button" onclick="alert('ffff')">tttttt</input></div></td>
-			<td class="col-md-3 span4" span=""><div style="font-size:small">${item.description}</div></td>						
+			<td>УНП</td>
+			<td><input id="unpInputId" type="text" class='simpleInput' /></td>
+			<td><a id="checkBtn" class="btn btn-primary">Проверить</a></td>
 		</tr>
-	</c:forEach>
-	</tbody>
-</table>
-<div class="paging"></div>
+	</table>
+</aui:form>
+<div id="notFoundResult" style="display: none">
+	<p>Организация не включена в реестр АНО</p>
+</div>
 
+
+	<table id="ano-table"
+		class="table table-bordered table-striped table-hover">	
+			<thead>
+				<tr>
+					<td>ID</td>
+					<td>Name</td>
+					<td>Company</td>
+					<td>Start Date</td>
+				</tr>
+			</thead>
+			<tbody>
+			</tbody>
+		</table>
+	</table>
+<portlet:resourceURL var="checkUnpUrl"></portlet:resourceURL>
 <aui:script>
-$('#newTech-table').datatable({
-    pageSize: 6,
-    sort: [true, true],
-    filters: [true, true],
-    filterText: 'Поиск...'
-}) ;
-</aui:script>
+$(document).ready(function() {
+	
+	
+	$('#checkBtn').click(function(){
+		$('#notFoundResult').css('display','none');
+		$('#foundResult').css('display','none');
+		var data = {"unp":$('#unpInputId').val()};  		  	
+		$.ajax({
+		   url: '${checkUnpUrl}',
+		   dataType: 'json',
+		   data: data,
+		   success: function(data) { LoadCustomers(data); }
+		 });
+		 
+		
+		
+	});
+});
 
+function LoadCustomers(data) {	
+	var tbody = $("#ano-table > tbody").html("");
+		for ( var i in data) {
+			var customer = data[i];			
+			var rowText = "<tr><td>" + customer['org']['nameRus'] + "</td><td>" + 1
+					+ "</td><td>" + 1 + "</td><td>" + 1 + "</td></tr>";
+			$(rowText).appendTo(tbody);			
+		}
+	}
+</aui:script>
 
